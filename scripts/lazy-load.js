b5a935d2ffdc04cf8581915a373253c4d291868d
aidanbmartin <github@aidanbmartin.com>
2024-02-21 21:43:13 -0500

    Deleted all images- fetching them from ImageKit.io instead. Implemented lazy loading with JS.

---
+++ scripts/lazy-load.js
@@ -0,0 +1,49 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var lazyLoadImages;
+
+    if ("IntersectionObserver" in window) {
+        lazyLoadImages = document.querySelectorAll(".lazy");
+        var imageObserver = new IntersectionObserver(function(entries, observer) {
+            entries.forEach(function(entry) {
+                if (entry.isIntersecting) {
+                    var image = entry.target;
+                    image.src = image.dataset.src;
+                    image.classList.remove("lazy");
+                    imageObserver.unobserve(image);
+                }
+            });
+        });
+
+        lazyLoadImages.forEach(function(image) {
+            imageObserver.observe(image);
+        });
+    } else {
+        var lazyLoadThrottleTimeout;
+        lazyLoadImages = document.querySelectorAll(".lazy");
+
+        function lazyLoad () {
+            if (lazyLoadThrottleTimeout) {
+                clearTimeout(lazyLoadThrottleTimeout);
+            }
+
+            lazyLoadThrottleTimeout = setTimeout(function() {
+                var scrollTop =  window.pageYOffset;
+                lazyLoadImages.forEach(function(img) {
+                    if(img.offsetTop < (window.innerHeight + scrollTop)) {
+                        img.src = img.dataset.src;
+                        img.classList.remove('lazy')
+                    }
+                });
+                if(lazyLoadImages.length == 0) {
+                    document.removeEventListener("scroll", lazyLoad);
+                    document.removeEventListener("resize", lazyLoad);
+                    document.removeEventListener("orientationChange", lazyLoad);
+                }
+            }, 20);
+        }
+
+        document.addEventListener("scroll", lazyLoad);
+        document.addEventListener("resize", lazyLoad);
+        document.addEventListener("orientationChange", lazyLoad);
+    }
+})
